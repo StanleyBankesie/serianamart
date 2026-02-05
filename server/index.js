5723785c2fc1252411affe58fb0f6de7afa058a1
STANLEY BANKESIE <stanleyetornam@gmail.com>
2026-02-05 17:14:32 +0000

    new changes

--- server/index.js
+++ server/index.js
@@ -42,6 +42,7 @@ console.log(
 /* ---------------- CORS ---------------- */
 const allowedOrigins = [
   "http://localhost:5173",
+  "http://localhost:8000",
   "http://127.0.0.1:5173",
   "https://serianamart.omnisuite-erp.com",
 ];
