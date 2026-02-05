e37f574beaf2fc0c3ae27e159cb7bad23fe1cbdf
STANLEY BANKESIE <stanleyetornam@gmail.com>
2026-02-05 17:17:27 +0000

    remove url

--- server/index.js
+++ server/index.js
@@ -42,7 +42,6 @@ console.log(
 /* ---------------- CORS ---------------- */
 const allowedOrigins = [
   "http://localhost:5173",
-  "http://localhost:8000",
   "http://127.0.0.1:5173",
   "https://serianamart.omnisuite-erp.com",
 ];
